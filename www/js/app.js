fa9b87d2873f1d11a3a165fb8e3e04999f64a9f4
keruitan <keruitan2018@u.northwestern.edu>
2018-01-29 22:00:19 -0600

    test

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,8 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -21,4 +23,23 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+
+
 })
+/*import { Camera, CameraOptions } from '@ionic-native/camera';
+    constructor(private camera: Camera) { };
+const options: CameraOptions = {
+  quality: 100,
+  destinationType: this.camera.DestinationType.DATA_URL,
+  encodingType: this.camera.EncodingType.JPEG,
+  mediaType: this.camera.MediaType.PICTURE
+}
+
+this.camera.getPicture(options).then((imageData) => {
+ // imageData is either a base64 encoded string or a file URI
+ // If it's base64:
+ let base64Image = 'data:image/jpeg;base64,' + imageData;
+}, (err) => {
+ // Handle error
+ console.log(err);
+});*/
